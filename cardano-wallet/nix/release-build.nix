7e4ddd5936f857cd61a004cd56fbd40c970b831d
Louis Bettens <louis@bettens.info>
2022-02-08 00:34:43 +0100

    git subrepo pull --remote=https://github.com/lourkeur/cardano-wallet cardano-wallet
    subrepo:
      subdir:   "cardano-wallet"
      merged:   "8cc8cb3b7"
    upstream:
      origin:   "https://github.com/lourkeur/cardano-wallet"
      branch:   "v2022-01-18"
      commit:   "8cc8cb3b7"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

--- cardano-wallet/nix/release-build.nix
+++ cardano-wallet/nix/release-build.nix
@@ -4,32 +4,25 @@
 ############################################################################
 
 { pkgs
-, gitrev  # git revision to stamp the executable with
 , exe  # executable component for the program, from Haskell.nix
 , backend ? null  # node backend
 }:
 
 with pkgs.lib;
 
-pkgs.stdenv.mkDerivation rec {
+let drv = pkgs.stdenv.mkDerivation rec {
   name = "${exe.identifier.name}-${version}";
   version = exe.identifier.version;
   phases = [ "installPhase" ];
-  nativeBuildInputs = with pkgs.buildPackages; [ iohk-nix-utils binutils nix ];
   installPhase = ''
     cp -R ${exe} $out
-    chmod -R +w $out
-    set-git-rev "${gitrev}" $out/bin/${exe.exeName}* || true
-  '' + (if (pkgs.stdenv.hostPlatform.isWindows && backend != null) then ''
+  '' + (optionalString (pkgs.stdenv.hostPlatform.isWindows && backend != null) ''
     # fixme: remove this
     cp -Rv ${backend.deployments} $out/deployments
-    # fixme: make sure dlls are added
-  '' else (if pkgs.stdenv.hostPlatform.isDarwin then ''
-    rewrite-libs $out/bin $out/bin/${exe.exeName}
-  '' else ''
-    $STRIP $out/bin/${exe.exeName}
-  ''));
-
+  '');
   meta.platforms = platforms.all;
-  passthru = optionalAttrs (backend != null) { inherit backend; };
-}
+  passthru = {
+    exePath = drv + "/bin/cardano-wallet";
+  } // (optionalAttrs (backend != null) { inherit backend; });
+};
+in drv
