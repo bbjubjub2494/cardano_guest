fb65e4118d7aa57bae972d6eeffc420a75d86bf8
Louis Bettens <louis@bettens.info>
2022-07-29 19:40:07 +0200

    remove dead code

--- nix/devshell.nix
+++
@@ -1,9 +0,0 @@
-{ mkShell, haskellPackages, haskell-nix, compiler-nix-name ? "ghc8107" }:
-
-mkShell {
-        buildInputs = [
-          haskell-nix.nix-tools.${compiler-nix-name}
-          haskellPackages.cabal-install
-          haskellPackages.ghc
-        ];
-      }
